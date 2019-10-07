dd589165b05b216b6bcaeaec55593a44cebd1fb9
nickgnd <nicolo.gnudi@gmail.com>
2019-10-07 23:25:24 +0200

    Handle todo creation

--- lib/live_todos_web/live/todo_live.ex
+++ lib/live_todos_web/live/todo_live.ex
@@ -5,11 +5,21 @@ defmodule LiveTodosWeb.TodoLive do
   alias LiveTodosWeb.TodoView
 
   def mount(_session, socket) do
-    {:ok, assign(socket, todos: Todos.list_todos())}
+    {:ok, fetch(socket)}
   end
 
   def render(assigns) do
     TodoView.render("todos.html", assigns)
+  end
+
+  def handle_event("add", %{"todo" => todo}, socket) do
+    Todos.create_todo(todo)
+
+    {:noreply, fetch(socket)}
+  end
+
+  defp fetch(socket) do
+    assign(socket, todos: Todos.list_todos())
   end
 end
 
